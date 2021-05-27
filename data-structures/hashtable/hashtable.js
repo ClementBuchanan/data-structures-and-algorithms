c925bd030f3b1e2ab5528f090599e18ec176c265
Clement Buchanan <clement.buchanan@gmail.com>
2021-05-26 23:03:22 -0400

    1st ACP for CC30

---
+++ data-structures/hashtable/hashtable.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const hash = (key, size) => {
+  let hashedKey = 0;
+  for (let i = 0; i < key.length; i++) {
+    hashedKey += key.charCodeAt(i);
+  }
+  return hashedKey % size;
+}
+
+class hashtable {
+  constructor() {
+    this.size = 20;
+    this.buckets = Array(this.size);
+
+    //populate each bucket wihth a map
+    for (let i = 0; i < this.buckets.length; i++) {
+      this.buckets[i] = new Map();
+    }
+  }
+
+  insert(key, value) {
+    let idx = hash(key, this.size);
+    this.buckets[idx].set(key, value);
+  }
+}
