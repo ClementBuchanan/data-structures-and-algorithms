e2c3a821d553d19872cde756eaffecbe5154c678
Clement Buchanan <clement.buchanan@gmail.com>
2021-06-28 15:58:44 -0400

    CC 15 updated and completed

--- tree/tree.js
+++ tree/tree.js
@@ -1,35 +1,32 @@
-'use strict';
+"use strict";
 
-'use strict';
-
-class BinarySearchTree {
-
+class BinaryTree {
   constructor() {
     this.root = null;
   }
 
   insertNode(val) {
-    var node = {
-      data: val,
+    const node = {
+      value: val,
       left: null,
-      right: null
+      right: null,
     };
 
-    var currentNode;
+    let currentNode;
 
     if (!this.root) {
       this.root = node;
     } else {
       currentNode = this.root;
       while (currentNode) {
-        if (val < currentNode.data) {
+        if (val < currentNode.value) {
           if (!currentNode.left) {
             currentNode.left = node;
             break;
           } else {
             currentNode = currentNode.left;
           }
-        } else if (val > currentNode.data) {
+        } else if (val > currentNode.value) {
           if (!currentNode.right) {
             currentNode.right = node;
             break;
@@ -37,24 +34,74 @@ class BinarySearchTree {
             currentNode = currentNode.right;
           }
         } else {
-          console.log('Ignoring this value as the BST supposed to be a tree containing UNIQUE values');
           break;
         }
       }
     }
   }
+
+  // depth first search algorythm - branch by branch.
+  //in-order. Process left node -> root node -> right node
+  //breath first search - level by level
+  //use a queue
+  InOrder() {
+    let result = [];
+
+    const traverse = (node) => {
+      // if left child exists, go left
+      if (node.left) traverse(node.left);
+      result.push(node.value);
+
+      //if righ child exists, go right
+      if (node.right) traverse(node.right);
+    };
+    traverse(this.root);
+    return result;
+  }
+
+  //pre-order. Process root node -> left node -> right node
+  PreOrder() {
+    let result = [];
+
+    const traverse = (node) => {
+      result.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    };
+    traverse(this.root);
+
+    return result;
+  }
+
+  //post-order. Process left node -> right node -> root node
+  PostOrder() {
+    let result = [];
+
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      result.push(node.value);
+    };
+    traverse(this.root);
+
+    return result;
+  }
+
+  //contains gets a value and check whether its in the tree
+  contains(value) {
+    let currentNode = this.root;
+
+    while (currentNode) {
+      if (value === currentNode.value);
+      return true;
+    }
+    if (value < currentNode.value) {
+      currentNode = currentNode.left;
+    } else {
+      currentNode = currentNode.right;
+    }
+    return false;
+  }
 }
 
-var BST = new BinarySearchTree();
-
-BST.insertNode(8);
-BST.insertNode(3);
-BST.insertNode(10);
-BST.insertNode(1);
-BST.insertNode(6);
-BST.insertNode(14);
-BST.insertNode(4);
-BST.insertNode(7);
-BST.insertNode(13);
-
-console.log(BST);
+module.exports = BinaryTree;
